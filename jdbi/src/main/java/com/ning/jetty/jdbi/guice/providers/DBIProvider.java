f11c82b791022e46701dc3404d7f4f85896e8ca9
Pierre-Alexandre Meyer <pierre@ning.com>
2012-06-15 16:38:47 -0700

    jdbi: don't use Log4j logger
    Add a bridge via slf4j.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- jdbi/src/main/java/com/ning/jetty/jdbi/guice/providers/DBIProvider.java
+++ jdbi/src/main/java/com/ning/jetty/jdbi/guice/providers/DBIProvider.java
@@ -16,37 +16,60 @@
 
 package com.ning.jetty.jdbi.guice.providers;
 
-import com.ning.jetty.jdbi.config.DaoConfig;
+import java.util.concurrent.TimeUnit;
+
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.TimingCollector;
+import org.skife.jdbi.v2.logging.FormattedLog;
+import org.skife.jdbi.v2.tweak.SQLLog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.jolbox.bonecp.BoneCPConfig;
 import com.jolbox.bonecp.BoneCPDataSource;
+import com.ning.jetty.jdbi.config.DaoConfig;
 import com.yammer.metrics.core.MetricsRegistry;
 import com.yammer.metrics.jdbi.InstrumentedTimingCollector;
 import com.yammer.metrics.jdbi.strategies.BasicSqlNameStrategy;
-import org.skife.jdbi.v2.DBI;
-import org.skife.jdbi.v2.TimingCollector;
-import org.skife.jdbi.v2.logging.Log4JLog;
-import org.skife.jdbi.v2.tweak.SQLLog;
 
-import java.util.concurrent.TimeUnit;
+public class DBIProvider implements Provider<DBI> {
+    private static final Logger logger = LoggerFactory.getLogger(DBIProvider.class);
 
-public class DBIProvider implements Provider<DBI>
-{
     private final MetricsRegistry metricsRegistry;
     private final DaoConfig config;
 
+    private static final class Slf4jLogging extends FormattedLog {
+        /**
+         * Used to ask implementations if logging is enabled.
+         *
+         * @return true if statement logging is enabled
+         */
+        @Override
+        protected boolean isEnabled() {
+            return true;
+        }
+
+        /**
+         * Log the statement passed in
+         *
+         * @param msg the message to log
+         */
+        @Override
+        protected void log(final String msg) {
+            logger.info(msg);
+        }
+    }
+
     @Inject
-    public DBIProvider(final MetricsRegistry metricsRegistry, final DaoConfig config)
-    {
+    public DBIProvider(final MetricsRegistry metricsRegistry, final DaoConfig config) {
         this.metricsRegistry = metricsRegistry;
         this.config = config;
     }
 
     @Override
-    public DBI get()
-    {
+    public DBI get() {
         final BoneCPConfig dbConfig = new BoneCPConfig();
         dbConfig.setJdbcUrl(config.getJdbcUrl());
         dbConfig.setUsername(config.getUsername());
@@ -63,7 +86,7 @@ public class DBIProvider implements Provider<DBI>
 
         final BoneCPDataSource ds = new BoneCPDataSource(dbConfig);
         final DBI dbi = new DBI(ds);
-        final SQLLog log = new Log4JLog();
+        final SQLLog log = new Slf4jLogging();
         dbi.setSQLLog(log);
 
         final BasicSqlNameStrategy basicSqlNameStrategy = new BasicSqlNameStrategy();
